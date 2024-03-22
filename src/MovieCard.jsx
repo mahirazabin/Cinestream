a65e16e229c8043638aec4005b9dd45f2081f2aa
mahirazabin <mahirazabin@gmail.com>
2024-03-22 00:32:11 -0600

    Fix the movie cards to dynamic

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,15 +1,25 @@
-// for react files, jsx is recommended
+import React from "react";
 
-import React from 'react';
+const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+  return (
+    <div className="movie" key={imdbID}>
+      <div>
+        <p>{Year}</p>
+      </div>
 
-const MovieCard = ({movie1}) => {
-    return (
-        <div className="movie">
-            <p>{movie1.Year}</p>
-        </div>
+      <div>
+        <img
+          src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"}
+          alt={Title}
+        />
+      </div>
 
-        <div>
-            <img src={movie1.poster !== 'N/A'? }
-        </div>
-    )
-}
+      <div>
+        <span>{Type}</span>
+        <h3>{Title}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
