a91c88fdbc3372a78d5fa65b6fc14026142885b2
mahirazabin <mahirazabin@gmail.com>
2024-03-22 00:46:17 -0600

    Name changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Batman");
+    searchMovies("Godzilla");
   }, []);
 
   const searchMovies = async (title) => {
@@ -23,7 +23,7 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>MovieLand</h1>
+      <h1>Cinestream</h1>
 
       <div className="search">
         <input
